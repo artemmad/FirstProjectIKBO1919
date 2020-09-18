85b547a588d6a7ffdbc51522fb304c276a5d7840
lavvlada <happybadrijan@gmail.com>
2020-09-18 11:28:51 +0300

    Решена первая практика

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -6,48 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*Scanner scanner = new Scanner(System.in);
 
-        //примитивные типы данных
-        int a = 10;//4 байта
-        float b = 20.7f; // 8-16 байта
-        boolean c = false; //1 байт
-
-        a = (int) b;
-        b = a;
-
-        // обьектные/ссылочные типы
-        int[] d = new int[10];
-        String string = new String("");
-        String string2 = scanner.nextLine();
-        String string3 = "dwadwadwa";
-
-        String[] stringArray = string2.split("  ");
-
-        for(String v : stringArray ){
-            System.out.println(v);
-        }*/
+		//Задача 1.3
+		int sum = 0;
+		int[] mass13 = {1,2,3,4};
+		for(int i : mass13){
+			summ+=i;
+		}
+		System.out.println("Cумма чисел:" + sum);
 
 
-	//Задача 1.3
-	int s = 0;
-	int[] ar13 = {1,2,3,4};
-	for(int i : ar13){
-	   s+=i;
-	}
-	System.out.print("Это сумма:" + s);
-	System.out.println();
-    
-	//Задача 1.4
+		//Задача 1.4
 
-	for(String i : args) {
-	   System.out.println("Принятый аргумент:" + i); 
+	for(String g : args) {
+	   System.out.println("Принятый аргумент:" + g);
 	}
 
 	//Задача 1.5
 	
-	for (int i = 1; i < 11; i++) {
-	    System.out.println("Член гармонического ряда:" + 1./i); 
+	for (int k = 1; k < 11; k++) {
+	    System.out.println("Член гармонического ряда:" + 1./k);
 	}
 
   	//Задача 1.6
@@ -65,15 +43,11 @@ public class Main {
     
     //Метод для задачи 1.7
     public static void factorCalc(int num) {
-	 int res = 1;
-	 int i = 1;
-	 for(i = 1; i <= num; i++ ) { res*= i; }
-	 System.out.println(res);
+	 int result= 1;
+	 int n = 1;
+	 for(n = 1; n <= num; n++ ) { result*= n; }
+	 System.out.println(result);
 	}
-
-			/*public static void sayHello(String name) {
-				System.out.println("Привет, " + name);
-			}*/
-    	//Задача 1.8 решалась так: git commit Main.java -m "Текст коммита"
-	//git push origin main, где origin это - git@github.com:Panikowsky-M/FirstProjectIKBO1919
-  }
+//Задача 1.8 решалась путем загрузки исходного кода в репозиторий командой
+	//Git Push Origin Main
+		  }
